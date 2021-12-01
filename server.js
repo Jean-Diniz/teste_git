0ee68ce78ce922ae4885ea8f20aa0c41157fc23f
Jean-Diniz <95376589+Jean-Diniz@users.noreply.github.com>
2021-12-01 16:45:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send({ message: 'Olá mundo estou funcionando!'});
 })
 
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Sou o brabo'});
+})
+
 app.listen(3001, () => {
     console.log("Api rodando na porta 3001")
 })
